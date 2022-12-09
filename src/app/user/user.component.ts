c7214f25ec7a6b0e081fb42bcf026cd4166be1e5
suthineew <wsuthinee18@gmail.com>
2022-12-09 17:30:07 +0700

    added activated users emitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  id: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
   }
 
-}
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+    });
+  }
+
+  onActivate() {
+    this.userService.activatedEmitter.next(true);
+  }}
